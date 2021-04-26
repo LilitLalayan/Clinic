02cc2edb2f80e2271ac545298932c8dbe4a310de
david grig <davidgrig88@gmail.com>
2021-04-26 13:10:53 +0400

    made navbar responsive with  toggle button, contacts correction of height, add some general styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
+import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import { Link } from "react-router-dom";
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 const useStyles = makeStyles({
   root: {
     flexGrow: 1,
+    backgroundColor: "gray",
   },
 });
 
@@ -27,7 +28,7 @@ function Nav() {
   };
 
   return (
-    <Paper>
+    <AppBar position="static" className={classes.root}>
       <Tabs
         value={value}
         onChange={handleChange}
@@ -62,7 +63,7 @@ function Nav() {
           to="/auth"
         />
       </Tabs>
-    </Paper>
+    </AppBar>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDRwXHjyzcXJv-Y8m-2WawHlnmUsSkI2T8",
+  authDomain: "dental-clinic-92719.firebaseapp.com",
+  projectId: "dental-clinic-92719",
+  storageBucket: "dental-clinic-92719.appspot.com",
+  messagingSenderId: "474374983626",
+  appId: "1:474374983626:web:472e38330486d40dff0933",
+};
+
+firebase.initializeApp(firebaseConfig);
+export const db = firebase.firestore();
+export const auth = firebase.auth();
+export const storage = firebase.storage();
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
