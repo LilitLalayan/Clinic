6f97760e5bdb274f463db25f943cb0a9afc94b70
david grig <davidgrig88@gmail.com>
2021-04-26 14:37:43 +0400

    made navbar responsive with toggle button, added some general styles, correction contacts page height

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,7 +1,73 @@
 import React from "react";
+import ContactsPhones from "./ContactsPhones"
+import ContactsAddress from "./ContactsAddress";
+import ContactsWorkingHours from "./ContactsWorkingHours";
+import ContactsSimpleMap from "./ContactsSimpleMap";
+import "@fontsource/roboto";
+import "./Contacts.css";
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  gridContainer: {
+    marginTop: "20px",
+    height: "80vh"
+  },
+  
+  gridItem: {
+    color: "#fff",
+    height: "100%",
+    backgroundColor: "#36ab35",
+    padding: "40px",
+    borderTopLeftRadius: "10px",
+    borderBottomLeftRadius: "10px",
+    borderTopRightRadius: "10px",
+    borderBottomRightRadius: "10px",
+    ['@media (min-width:960px)']: { // eslint-disable-line no-useless-computed-key
+      borderTopRightRadius: "0",
+      borderBottomRightRadius: "0",
+    }
+  },
+});
+
 
 function Contacts() {
-  return <div>Contacts</div>;
+  const classes = useStyles();
+
+  return (<div className="contacts">
+    <Container fixed >
+      <Grid container justify="center" className={classes.gridContainer}>
+        <Grid item lg={6} md={7} xs={12} className={classes.gridItem}>
+          <Grid container>
+            <Grid container>
+              <Grid item md={8} sm={8} xs={8}>
+                <ContactsWorkingHours />
+              </Grid>
+              <Grid item md sm={4} xs={4}>
+                <ContactsAddress />
+              </Grid>
+            </Grid>
+            <Grid container justify="center">
+              
+            </Grid>
+            <Grid container>
+              <Grid item md sm={8} xs={10}>
+                <ContactsPhones />
+              </Grid>
+            </Grid> 
+          </Grid>
+        </Grid>
+       
+        <Grid item lg={6} md={5} xs={12}>
+          <div className="contacts__map">
+            < ContactsSimpleMap />
+          </div>
+           
+        </Grid>
+      </Grid>
+    </Container>   
+  </div>);
 }
 
 export default Contacts;
