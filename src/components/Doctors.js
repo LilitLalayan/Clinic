65453b70eca47717cb4e064e54cc2da9a0879cc7
valeriavetyan <avetyan.valeri@mail.ru>
2021-04-28 19:12:18 +0400

    Finished version 1

--- src/components/Doctors.js
+++ src/components/Doctors.js
@@ -1,22 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-
-import DocImage1 from "./DoctorsImages/DocImg1.jpg";
-import DocImage2 from "./DoctorsImages/DocImg2.jpg";
-import DocImage3 from "./DoctorsImages/DocImg3.jpg";
-import DocImage4 from "./DoctorsImages/DocImg4.jpg";
-import DocImage5 from "./DoctorsImages/DocImg5.jpg";
-import DocImage6 from "./DoctorsImages/DocImg6.jpg";
-
 import { db } from "..";
 import { Container, Grid } from "@material-ui/core";
+import Doctor from "./Doctor";
 
 const useStyles = makeStyles({
   root: {
@@ -31,202 +17,38 @@ const useStyles = makeStyles({
     height: 300,
     maxWidth: 320,
   },
+  // asdd: {
+  //   // backgroundImage: `url("https://image.freepik.com/free-vector/dentist-medical-background-with-3d-tooth-design_1017-26095.jpg")`,
+  // },
 });
 
 export default function Doctors() {
   const classes = useStyles();
 
   const [allDocotors, setAllDoctors] = useState([]);
-  console.log(allDocotors);
+
+  console.log(allDocotors, 1);
   useEffect(() => {
+    // retrieving doctors data from firebase-database
     const doctorsRef = db.collection("doctors");
     doctorsRef.get().then((querySnapShot) => {
       const data = [];
-      querySnapShot.forEach((asd) => {
-        data.push(asd.data());
+      querySnapShot.forEach((snapShotData) => {
+        data.push(snapShotData.data());
       });
       setAllDoctors(data);
     });
   }, []);
 
   return (
-    <div>
+    <div className={classes.asdd}>
       <Container className={classes.cardGrid}>
         <Grid container spacing={4}>
-          <Grid className={classes.gridItem}>
-            <Card className={classes.root}>
-              <CardActionArea>
-                <CardMedia
-                  className={classes.media}
-                  image={DocImage1}
-                  title="Contemplative Reptile"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Dr. Levon Asatryan
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    some text some text some text some text some text some text
-                    some text some text some text some text some text some text
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button size="small" color="primary">
-                  make an appointment
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-          <Grid className={classes.gridItem}>
-            <Card className={classes.root}>
-              <CardActionArea>
-                <CardMedia
-                  className={classes.media}
-                  image={DocImage2}
-                  title="Contemplative Reptile"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Dr. Tatev Sahakyan
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    some text some text some text some text some text some text
-                    some text some text some text some text some text some text
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button size="small" color="primary">
-                  make an appointment
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-          <Grid className={classes.gridItem}>
-            <Card className={classes.root}>
-              <CardActionArea>
-                <CardMedia
-                  className={classes.media}
-                  image={DocImage3}
-                  title="Contemplative Reptile"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Dr. Ani Margaryan
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    some text some text some text some text some text some text
-                    some text some text some text some text some text some text
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button size="small" color="primary">
-                  make an appointment
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-          <Grid className={classes.gridItem}>
-            <Card className={classes.root}>
-              <CardActionArea>
-                <CardMedia
-                  className={classes.media}
-                  image={DocImage4}
-                  title="Contemplative Reptile"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Dr. Lilit Galstyan
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    some text some text some text some text some text some text
-                    some text some text some text some text some text some text
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button size="small" color="primary">
-                  make an appointment
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-          <Grid className={classes.gridItem}>
-            <Card className={classes.root}>
-              <CardActionArea>
-                <CardMedia
-                  className={classes.media}
-                  image={DocImage5}
-                  title="Contemplative Reptile"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Dr. Narek Petrosyan
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    some text some text some text some text some text some text
-                    some text some text some text some text some text some text
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button size="small" color="primary">
-                  make an appointment
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-          <Grid className={classes.gridItem}>
-            <Card className={classes.root}>
-              <CardActionArea>
-                <CardMedia
-                  className={classes.media}
-                  image={DocImage6}
-                  title="Contemplative Reptile"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Dr. Alen Markosyan
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    some text some text some text some text some text some text
-                    some text some text some text some text some text some text
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button size="small" color="primary">
-                  make an appointment
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
+          {allDocotors.map((doctor, index) => {
+            return (
+              <Doctor doctor={doctor} key={doctor.uid} index={index + 1} />
+            );
+          })}
         </Grid>
       </Container>
     </div>
