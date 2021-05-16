37d207f6235e72b3063bbed15ac68e0a8da93f1d
LilitLalayan <76946852+LilitLalayan@users.noreply.github.com>
2021-05-16 21:22:19 +0400

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -208,8 +208,7 @@ function Profile() {
           .updateEmail(newEmail)
           .then(function () {
             alert("You have successfully changed your email");
-            console.log(777);
-            dispatch({
+              dispatch({
               type: SET_LOGGEDIN_USER,
               user: {
                 ...loggedInUser,
